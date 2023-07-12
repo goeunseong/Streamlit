2ece3494eee7d26aff4a072c46ea91e97fbafa08
goeunseong <69621732+goeunseong@users.noreply.github.com>
2023-07-12 16:01:34 +0900

    Update ab_test.py

--- ab_test.py
+++ ab_test.py
@@ -132,14 +132,6 @@ def test(a,b, text):
     st.write('<p style="font-family:sans-serif; font-size:15px; color:blue;">단측 검정 결과  ', f' ▷ {direction}</p>', unsafe_allow_html=True) 
     st.write(one_s_test)
 
-st.write("""**업로드할 파일은 첫 행이 컬럼명이어야 하고, 컬럼 구성은 다음과 같이 부탁드릴게요:**)  
-         - 날짜 : 소재 노출일자  
-         - 소재명 : 각 소재의 이름  
-         - 소재 구분 : 시안1(대조군)과 시안2(실험군) 구분값을 넣은 컬럼이에요. 0과 1로 구분해주세요.  
-         - 노출 : 소재 노출수  
-         - 클릭 : 소재 클릭수  
-         - 앱 설치 : 앱 설치수 """)
-
 with st.sidebar:
     choose = option_menu("AB 테스트", ["About", "A/B 테스트", "Update log"],
                          icons=['house', 'cpu fill', 'kanban'],
@@ -155,6 +147,13 @@ with st.sidebar:
 if choose == 'A/B 테스트':
     # 앱 메인 타이틀
     st.title('A/B 테스트 for creative optimization')
+    st.write("""**업로드할 파일은 첫 행이 컬럼명이어야 하고, 컬럼 구성은 다음과 같이 부탁드릴게요:**)  
+         - 날짜 : 소재 노출일자  
+         - 소재명 : 각 소재의 이름  
+         - 소재 구분 : 시안1(대조군)과 시안2(실험군) 구분값을 넣은 컬럼이에요. 0과 1로 구분해주세요.  
+         - 노출 : 소재 노출수  
+         - 클릭 : 소재 클릭수  
+         - 앱 설치 : 앱 설치수 """)
     # 파일 업로드 버튼(업로드 기능)
     file = st.file_uploader('파일 선택(csv or excel)', type=['csv', 'xls', 'xlsx'])
     if file is not None:
